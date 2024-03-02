9b859e1d6622bfd4577af48df2ea682034c58b5f
viktorsdk <datakobs0@gmail.com>
2024-03-02 13:51:04 +0100

    cover all test cases

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -8,4 +8,20 @@ describe("fizzbuzz", () => {
     it("returns Buzz if the number is 5", () => {
         expect(fizzbuzz(5)).toBe("Buzz");
     })
+
+    it("returns FizzBuzz if the number is 15", () => {
+        expect(fizzbuzz(15)).toBe("FizzBuzz");
+    })
+
+    it("throws an error if the number is 0", () => {
+        expect(() => fizzbuzz(0)).toThrowError("Number must be greater than 0");
+    })
+
+    it("throws an error if the number is larger than 100", () => {
+        expect(() => fizzbuzz(101)).toThrowError("Number must be less than 100");
+    })
+
+    it ("throws an error if the input is not a number", () => {
+        expect(() => fizzbuzz("hello" as unknown as number)).toThrowError("Input must be a number");
+    })
 });

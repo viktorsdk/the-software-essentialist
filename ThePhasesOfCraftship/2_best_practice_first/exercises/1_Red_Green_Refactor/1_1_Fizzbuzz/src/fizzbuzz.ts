a2ff7cc4506c9b65866db9203c07856ce3dcdd96
viktorsdk <datakobs0@gmail.com>
2024-03-02 14:08:26 +0100

    refactor fizzbuzz test

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,21 +1,9 @@
 export const fizzbuzz = (input: number): string|undefined => {
-    if (typeof input !== "number") {
-        throw new Error("Input must be a number");
-    }
-    if (input < 1) {
-        throw new Error("Number must be greater than 0");
-    }
-    if (input > 100) {
-        throw new Error("Number must be less than 100");
-    }
-    if (input % 3 === 0 && input % 5 === 0) {
-        return "FizzBuzz";
-    }
-    if (input % 3 === 0) {
-        return "Fizz";
-    }
-    if (input % 5 === 0) {
-        return "Buzz";
-    }
+    if (typeof input !== "number") throw new Error("Input must be a number")
+    if (input < 1 || input > 100) throw new Error("Number must be greater than 0")
+    if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz";
+    if (input % 3 === 0) return "Fizz";
+    if (input % 5 === 0) return "Buzz";
 
+    return input.toString();
 }
--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,27 +1,28 @@
 import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-    it("returns Fizz if the number is 3", () => {
-        expect(fizzbuzz(3)).toBe("Fizz");
-    });
-
-    it("returns Buzz if the number is 5", () => {
-        expect(fizzbuzz(5)).toBe("Buzz");
+    // refactor using it.each()
+    it.each([3, 6, 9, 12, 18, 21, 24, 27, 33, 36, 39, 42, 48, 51, 54, 57, 63, 66, 69, 72, 78, 81, 84, 87, 93, 96, 99])("returns Fizz if the number is %i", (number) => {
+        expect(fizzbuzz(number)).toBe("Fizz");
     })
 
-    it("returns FizzBuzz if the number is 15", () => {
-        expect(fizzbuzz(15)).toBe("FizzBuzz");
+    it.each([5, 10, 20, 25, 35, 40, 50, 55, 65, 70, 80, 85, 95, 100])("returns Buzz if the number is %i", (number) => {
+        expect(fizzbuzz(number)).toBe("Buzz");
     })
 
-    it("throws an error if the number is 0", () => {
-        expect(() => fizzbuzz(0)).toThrowError("Number must be greater than 0");
+    it.each([15, 30, 45, 60, 75, 90])("returns FizzBuzz if the number is %i", (number) => {
+        expect(fizzbuzz(number)).toBe("FizzBuzz");
     })
 
-    it("throws an error if the number is larger than 100", () => {
-        expect(() => fizzbuzz(101)).toThrowError("Number must be less than 100");
+    it.each([0, 101])("throws an error if the number is %i", (number) => {
+        expect(() => fizzbuzz(number)).toThrowError("Number must be greater than 0");
     })
 
-    it ("throws an error if the input is not a number", () => {
-        expect(() => fizzbuzz("hello" as unknown as number)).toThrowError("Input must be a number");
+    it.each(["hello" as unknown as number])("throws an error if the input is not a number", (number) => {
+        expect(() => fizzbuzz(number)).toThrowError("Input must be a number");
+    })
+
+    it.each([1, 2, 4, 7, 8, 11, 13, 14, 16, 17, 19, 22, 23, 26, 28, 29, 31, 32, 34, 37, 38, 41, 43, 44, 46, 47, 49, 52, 53, 56, 58, 59, 61, 62, 64, 67, 68, 71, 73, 74, 76, 77, 79, 82, 83, 86, 88, 89, 91, 92, 94, 97, 98])("returns the number if the number is %i", (number) => {
+        expect(fizzbuzz(number)).toBe(number.toString());
     })
 });
